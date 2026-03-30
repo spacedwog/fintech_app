14f0f6ce936cba56b38985727c1e000b444f4d7e
Spacedwog <owner@spacedwog.com.br>
2026-03-30 16:53:30 -0300

    Commited

--- models/expense.py
+++
@@ -1,6 +0,0 @@
-class Expense:
-    def __init__(self, user_id, category_id, valor, data):
-        self.user_id = user_id
-        self.category_id = category_id
-        self.valor = valor
-        self.data = data
--- utils/devops.py
+++
@@ -1,6 +0,0 @@
-import logging
-
-logging.basicConfig(filename="app.log", level=logging.INFO)
-
-def log_event(msg):
-    logging.info(msg)
